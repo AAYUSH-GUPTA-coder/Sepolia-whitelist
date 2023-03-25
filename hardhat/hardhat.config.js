4e37e29647cdc42abad698d4c5bf0fbc8a92ca35
Aayush Gupta <guptaaayush432@gmail.com>
2023-03-25 23:21:16 +0530

    corrected typos

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const QUICKNODE_SEPOLIA_API_KEY_URL = process.env.QUICKNODE_SEPOLIA_API_KEY_URL;
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url: QUICKNODE_SEPOLIA_API_KEY_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_KEY,
+    },
+  },
 };
